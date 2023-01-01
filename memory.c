dde14de2d16de07c6b4bec80c3ea2188b5526217
C3ylon <1104236651@qq.com>
2023-01-01 21:54:16 +0800

    remove log.c.

--- memory.c
+++ memory.c
@@ -1,2 +1,16 @@
 #include "memory.h"
 
+DWORD pid;
+HANDLE hProcess;
+
+static BOOL ISINITIALIZED;
+
+BOOL Initialize(DWORD _pid)
+{
+    pid = _pid;
+    hProcess = OpenProcess(PROCESS_ALL_ACCESS, 0, pid);
+    if(hProcess) {
+        ISINITIALIZED = 1;
+    }
+    return ISINITIALIZED;
+}
--- memory.h
+++ memory.h
@@ -4,6 +4,10 @@
 #include <stdlib.h>
 #include <Windows.h>
 
+extern DWORD pid;
+extern HANDLE hProcess;
+
+int InitByPid(DWORD pid);
 
 
 #endif
