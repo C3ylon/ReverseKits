dce04cae547bef7377befe16d64af93dd513fa89
C3ylon <C3ylon@outlook.com>
2023-12-28 01:31:40 +0800

    new.

--- main.cpp
+++ main.cpp
@@ -9,7 +9,11 @@ using std::endl;
 
 
 int main() {
-    clre::FileMng fp("./test.txt", "wb");
-    fwrite("avbde", 1, 5, fp);
+    clre::FileMng fp("./test.txt", "rb");
+    _fseeki64(fp, 1, 0);
+    // fread(0, 1,1,fp);
+    char ch = 0;
+    fread(&ch, 1,1,fp);
+    cout << ch<<endl;
     return 0;
 }
