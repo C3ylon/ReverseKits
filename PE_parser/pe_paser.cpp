e2d871447784aab162a74cb6ab789fe852d4839b
C3ylon <C3ylon@outlook.com>
2023-12-27 19:22:44 +0800

    fix.

--- PE_parser/pe_paser.cpp
+++ PE_parser/pe_paser.cpp
@@ -5,6 +5,8 @@
 
 #include <windows.h>
 
+using std::cout;
+using std::endl;
 using std::string;
 using std::vector;
 
@@ -16,20 +18,47 @@ string printmemory(void *addr, size_t size) {
     string res;
     char ch[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
     for(size_t i = 0; i < size; i++) {
-        res += ch[((char*)addr)[i] / 16];
-        res += ch[((char*)addr)[i] % 16];
-        res += ' ';
+        res += ch[((unsigned char*)addr)[i] / 16];
+        res += ch[((unsigned char*)addr)[i] % 16];
+        if(i % 16 == 15)
+            res += '\n';
+        else
+            res += ' ';
     }
     res.pop_back();
     return res;
 }
 
-void parse_dos_header() {
+void output() {
+    auto size = printbuffer.size();
+    for(decltype(size) i = 0; i < size; i++) {
+        std::cout << printbuffer[i] << "\n";
+    }
+}
+
+void parse_dos_header(DWORD &e_lfanew) {
     WORD e_magic = 0;
     fread(&e_magic, 2, 1, fp);
     if(e_magic != 0x5A4D) {
         throw string("[!]dos header magic error: ") + printmemory(&e_magic, 2);
     }
+    printbuffer.push_back("[*]DOS header:");
+    _fseeki64(fp, 0x3C, 0);
+    fread(&e_lfanew, 4, 1, fp);
+    printbuffer.push_back(string("e_magic: ") + printmemory(&e_magic, 2));
+    printbuffer.push_back(string("e_lfanew: ") + printmemory(&e_lfanew, 4));
+}
+
+void parse_nt_header(DWORD e_lfanew) {
+    printbuffer.push_back(string(30, '='));
+    DWORD Signature = 0;
+    _fseeki64(fp, e_lfanew, 0);
+    fread(&Signature, 4, 1, fp);
+    if(Signature != 0x4550) {
+        throw string("[!]nt header magic error: ") + printmemory(&Signature, 4);
+    }
+    printbuffer.push_back("[*]NT header:");
+    printbuffer.push_back(string("Signature: ") + printmemory(&Signature, 4));
 }
 
 int main(int argc, char *argv[]) {
@@ -39,7 +68,10 @@ int main(int argc, char *argv[]) {
             throw string("[!]Missing parameter: file_path");
         }
         fp = clre::FileMng(argv[1], "rb");
-        parse_dos_header();
+        DWORD e_lfanew = 0;
+        parse_dos_header(e_lfanew);
+        parse_nt_header(e_lfanew);
+        output();
     } catch (const string &e) {
         std::cout << e << "\n";
     }
